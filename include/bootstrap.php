0f4021edd1ccca7b5d656168541c42e67b588b4c
Tomasz Bajorek <tbajorek3@gmail.com>
2016-12-09 11:39:14 +0100

    Remove session_start()
    An own session creating has removed. Plugin will be use built-in session system.

--- include/bootstrap.php
+++ include/bootstrap.php
@@ -51,23 +51,4 @@ function dotpay_autoloader($className) {
     require_once($path);
 }
 
-/**
- * Checks, if session is already started
- * @return boolean
- */
-function is_session_started()
-{
-    if ( php_sapi_name() !== 'cli' ) {
-        if ( version_compare(phpversion(), '5.4.0', '>=') ) {
-            return session_status() === PHP_SESSION_ACTIVE ? TRUE : FALSE;
-        } else {
-            return session_id() === '' ? FALSE : TRUE;
-        }
-    }
-    return FALSE;
-}
-
-if(!is_session_started())
-    session_start();
-
 spl_autoload_register('dotpay_autoloader');
